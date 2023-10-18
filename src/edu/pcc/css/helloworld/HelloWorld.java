d2fb90e54f692a25099a6b3bcb8a3157b93303d1
NathanNepple <nathan.nepple15@pcc.edu>
2023-10-17 17:33:46 -0700

    Added count to 5 feature

--- src/edu/pcc/css/helloworld/HelloWorld.java
+++ src/edu/pcc/css/helloworld/HelloWorld.java
@@ -6,5 +6,12 @@ public class HelloWorld {
     public static void main (String []args){
         System.out.println("Hello, World!");
         System.out.println (new Date());
+
+        int countTo = 5; //counts to 5
+            for (int i = 1; i <= countTo; i++) {
+                System.out.println(i);
+            }
     }
+
+
 }
