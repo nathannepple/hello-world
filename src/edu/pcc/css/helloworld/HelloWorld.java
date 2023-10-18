eb06b410a6cce6c83b8f2fa2700a53979a012e4d
NathanNepple <nathan.nepple15@pcc.edu>
2023-10-17 17:42:10 -0700

    added author name

--- src/edu/pcc/css/helloworld/HelloWorld.java
+++ src/edu/pcc/css/helloworld/HelloWorld.java
@@ -1,7 +1,12 @@
+
 package edu.pcc.css.helloworld;
 
 import java.util.Date;
 
+/**
+ * Prints hello world, date, and counts to 5
+ * @author Nathan Nepple
+ */
 public class HelloWorld {
     public static void main (String []args){
         System.out.println("Hello, World!");
