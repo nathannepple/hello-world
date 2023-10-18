9ce2711f5e3a09a7679eff86fc5eb0769d5aefb2
NathanNepple <nathan.nepple15@pcc.edu>
2023-10-17 17:16:58 -0700

    print date

--- src/edu/pcc/css/helloworld/HelloWorld.java
+++ src/edu/pcc/css/helloworld/HelloWorld.java
@@ -1,7 +1,10 @@
 package edu.pcc.css.helloworld;
 
+import java.util.Date;
+
 public class HelloWorld {
     public static void main (String []args){
         System.out.println("Hello, World!");
+        System.out.println (new Date());
     }
 }
